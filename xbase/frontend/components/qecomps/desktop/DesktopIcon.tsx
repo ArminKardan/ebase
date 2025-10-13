128f6eed19a7cf5c12b86b3079bbcd342ba20ac1
Saman Esmaellpour <samanes0011@gmail.com>
2025-10-13 13:04:35 +0330

    Update DesktopIcon.tsx

--- xbase/frontend/components/qecomps/desktop/DesktopIcon.tsx
+++ xbase/frontend/components/qecomps/desktop/DesktopIcon.tsx
@@ -1,7 +1,7 @@
 export default function DesktopIcon(props: { id: any, icon: any, label: any, onOpen: any }) {
     return (
         <c-cc
-            onDoubleClick={() => props.onOpen(props.id)}
+            onClick={() => props.onOpen(props.id)}
             className="gap-1 p-2 rounded hover:bg-white/20 cursor-pointer transition-colors w-20"
         >
             <c-cc className="w-12 h-12 bg-gradient-to-br from-blue-400 to-blue-600 rounded-lg text-white shadow-lg">
